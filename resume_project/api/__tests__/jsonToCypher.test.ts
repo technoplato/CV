87a1168c567dd1cdce62060a63147cae1571d845
Michael Lustig <lustig@knophy.com>
2023-07-07 11:20:52 -0400

    Testing via jest setup

---
+++ resume_project/api/__tests__/jsonToCypher.test.ts
@@ -0,0 +1,13 @@
+// import { jsonToCypher } from '../jsonToCypher'; // replace with the path to your function
+
+describe("jsonToCypher", () => {
+  test("correctly converts JSON to Cypher", () => {
+    const json = {
+      /* ... your test data ... */
+    };
+    // const expectedCypher = /* ... the expected Cypher ... */;
+    // const actualCypher = jsonToCypher(json);
+    // expect(actualCypher).toEqual(expectedCypher);
+    expect(true).toBe(false);
+  });
+});
